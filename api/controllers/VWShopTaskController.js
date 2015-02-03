a4e7f9b112fe174b68651d38e4df4533c5b594e5
morgzillac <mcheng@hyperbridge.com>
2015-02-03 13:44:14 +0800

    added pending and assigned

--- api/controllers/VWShopTaskController.js
+++ api/controllers/VWShopTaskController.js
@@ -4,6 +4,8 @@
  * @description :: Server-side logic for managing Vwshoptasks
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
+var util = require('util'),
+  actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
 
 module.exports = {
 // read only model, edit through Trans controller
@@ -23,7 +25,56 @@ module.exports = {
 
     console.log("not allowed");
     res.forbidden();
+  },
+
+
+ pending: function (req, res) {
+
+   var criteria = actionUtil.parseCriteria(req);
+   criteria.where = criteria.where || {};
+   criteria.where.pending = { '>': 0 };;
+
+   var query = VWShopTask.find()
+    .where( criteria )
+    .limit( actionUtil.parseLimit(req) )
+    .skip( actionUtil.parseSkip(req) )
+    .sort( actionUtil.parseSort(req) );
+
+   query.exec(function(err, results) {
+    if (err) res.customError('500', sails.config.errs.systemError('数据库错误'));
+
+    if (results) {
+      res.json(results);
+//        res.jsonx(results);
+    } else {
+      res.customError('500', sails.config.errs.db_userdata_not_found);
+    }
+  });
+
+},
+
+  assigned: function (req, res) {
+    var criteria = actionUtil.parseCriteria(req);
+    criteria.where = criteria.where || {};
+    criteria.where.pending = { '>': 0 };
+    var query = VWShopTask.find()
+      .where(criteria)
+      .limit(actionUtil.parseLimit(req))
+      .skip(actionUtil.parseSkip(req))
+      .sort(actionUtil.parseSort(req));
+
+    query.exec(function (err, results) {
+      if (err) res.customError('500', sails.config.errs.systemError('数据库错误'));
+
+      if (results) {
+        res.json(results);
+//        res.jsonx(results);
+      } else {
+        res.customError('500', sails.config.errs.db_userdata_not_found);
+      }
+    });
   }
 
-};
+
+  };
 
--- api/models/ShopTask.js
+++ api/models/ShopTask.js
@@ -7,7 +7,7 @@
 
 module.exports = {
   "schema": true,
-  migrate: 'drop',
+  //migrate: 'drop',
   "attributes": {
     "taskId": {
       "columnName": "task_id",
