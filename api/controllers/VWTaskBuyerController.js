f758162522f76d3da8b9e66ef632b2b3a8d0e4bf
morgzillac <mcheng@hyperbridge.com>
2015-02-05 00:37:20 +0800

    added buyer task list

---
+++ api/controllers/VWTaskBuyerController.js
@@ -0,0 +1,89 @@
+/**
+ * VWTaskBuyerController
+ *
+ * @description :: Server-side logic for managing Vwtaskbuyers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+var util = require('util'),
+  actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
+
+module.exports = {
+// read only model, edit through Trans controller
+  create: function (req, res) {
+
+    console.log("not allowed");
+    res.forbidden();
+  },
+
+  update: function (req, res) {
+
+    console.log("not allowed");
+    res.forbidden();
+  },
+
+  destroy: function (req, res) {
+
+    console.log("not allowed");
+    res.forbidden();
+  },
+
+
+  pending: function (req, res) {
+
+    var criteria = actionUtil.parseCriteria(req);
+    criteria.pending = { '>': 0 };
+    var query = VWTaskBuyer.find()
+      .where(criteria)
+      .limit(actionUtil.parseLimit(req))
+      .skip(actionUtil.parseSkip(req))
+      .sort(actionUtil.parseSort(req));
+
+    console.log("criteria = " + JSON.stringify(criteria));
+
+    query.exec(function(err, results) {
+      if (err) res.customError('500', sails.config.errs.systemError('数据库错误'));
+
+      if (results) {
+        res.json(results);
+//        res.jsonx(results);
+      } else {
+        res.customError('500', sails.config.errs.db_userdata_not_found);
+      }
+    });
+
+  },
+
+  assigned: function (req, res) {
+    var criteria = actionUtil.parseCriteria(req);
+    criteria.pending = { '>': 0 };
+    var query = VWTaskBuyer.find()
+      .where(criteria)
+      .limit(actionUtil.parseLimit(req))
+      .skip(actionUtil.parseSkip(req))
+      .sort(actionUtil.parseSort(req));
+
+    console.log("criteria = " + JSON.stringify(criteria));
+    criteria.where.pending = { '>': 0 };
+    var query = VWTaskBuyer.find()
+      .where(criteria.where)
+      .limit(actionUtil.parseLimit(req))
+      .skip(actionUtil.parseSkip(req))
+      .sort(actionUtil.parseSort(req));
+
+    console.log("criteria = " + JSON.stringify(criteria));
+    query.exec(function (err, results) {
+      if (err) res.customError('500', sails.config.errs.systemError('数据库错误'));
+
+      if (results) {
+        res.json(results);
+//        res.jsonx(results);
+      } else {
+        res.customError('500', sails.config.errs.db_userdata_not_found);
+      }
+    });
+  }
+
+
+};
+
---
+++ api/models/VWTaskBuyer.js
@@ -0,0 +1,109 @@
+/**
+* vTaskBuyer.js
+*
+* @description :: TODO: Write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  "schema": true,
+  migrate: 'safe',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  autoPK: false,
+
+      "attributes": {
+    "taskBuyerId": {
+      "columnName": "task_buyer_id",
+      "required": true,
+      "defaultsTo": "0",
+      "type": "integer"
+    },
+    "userId": {
+      "columnName": "user_id",
+      "type": "integer"
+    },
+    "taskId": {
+      "columnName": "task_id",
+      "type": "integer"
+    },
+    "buyerStatusId": {
+      "columnName": "buyer_status_id",
+      "type": "integer"
+    },
+    "commission": {
+      "type": "integer"
+    },
+    "bonus": {
+      "type": "integer"
+    },
+    "terminal": {
+      "type": "string",
+      "size": 45
+    },
+    "taskStatus": {
+      "columnName": "task_status",
+      "type": "string",
+      "size": 45
+    },
+    "taskComment": {
+      "columnName": "task_comment",
+      "type": "string",
+      "size": 255
+    },
+    "productId": {
+      "columnName": "product_id",
+      "type": "integer"
+    },
+    "productPrice": {
+      "columnName": "product_price",
+      "type": "integer"
+    },
+    "platformId": {
+      "columnName": "platform_id",
+      "type": "integer"
+    },
+    "taskTypeId": {
+      "columnName": "task_type_id",
+      "type": "integer"
+    },
+    "shopPrice": {
+      "columnName": "shop_price",
+      "type": "integer"
+    },
+    "productExtId": {
+      "columnName": "product_ext_id",
+      "type": "string",
+      "size": 50
+    },
+    "productName": {
+      "columnName": "product_name",
+      "type": "string",
+      "size": 255
+    },
+    "productDesc": {
+      "columnName": "product_desc",
+      "type": "string",
+      "size": 2000
+    },
+    "productImage": {
+      "columnName": "product_image",
+      "type": "string",
+      "size": 500
+    },
+    "productUrl": {
+      "columnName": "product_url",
+      "type": "string",
+      "size": 500
+    },
+    "productStatusId": {
+      "columnName": "product_status_id",
+      "type": "integer"
+    },
+    "createdAt": {
+      "columnName": "created_at",
+      "type": "datetime"
+    }
+  },
+  "tableName": "v_task_buyer"
+}
