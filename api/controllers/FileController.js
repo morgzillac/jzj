44de9f4bc272354488260d9ceac6354b6708a263
morgzillac <mcheng@hyperbridge.com>
2015-03-21 16:40:01 +0800

    fixed file upload and csv

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -20,9 +20,18 @@ module.exports = {
   },
 
   upload: function  (req, res) {
-    var type = req.param('type');
-    var uploadPath = '../../assets/uploads/' + type;
+    var arrTypes = [
+      {id:1,name:"userImg",remark:"头像信息和账号截图信息"},
+      {id:2,name:"taskImg",remark:"任务截图信息"},
+      {id:3,name:"productImg",remark:"产品截图信息"},
+      {id:4,name:"otherImg",remark:"其他"}
+    ];
 
+    var type = parseInt(req.param('type'));
+    if (isNaN(type) || type < 1 || type > 4) {
+      type = 4;
+    }
+    var uploadPath = '../../assets/uploads/' + arrTypes[type-1].name;
     if (req.method.toUpperCase()=='GET') return res.serverError('文件提交错误');
     req.file('thefile').upload({ dirname: uploadPath },function (err, files) {
       if (err)
